09b143eec5fd29807be5c30b9a9e73b3f96a6311
Rodrigo Pereira <rodrigopbbr@gmail.com>
2021-10-27 18:47:05 -0300

    Primeiro Commit Front

---
+++ pages/index.vue
@@ -0,0 +1,73 @@
+<template>
+ <div class="w-full bg-gray-800">
+    <section class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 py-12">
+        <div class="text-center pb-12">
+          
+            <h1 class="font-bold text-3xl md:text-4xl lg:text-5xl font-heading text-white">
+                Ambiente Teste            
+            </h1>
+        </div>
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            <div class="w-full bg-gray-900 rounded-lg sahdow-lg p-12 flex flex-col justify-center items-center">
+                <div class="mb-8">
+                    <img class="object-center object-cover rounded-full h-36 w-36" src="" alt="photo">
+                    
+                </div>
+                <div class="text-center">
+                    <p class="text-xl text-white font-bold mb-2">D1</p>
+                    <p class="text-base text-gray-400 font-normal"></p>
+                </div>
+            </div>
+            <div class="w-full bg-gray-900 rounded-lg sahdow-lg p-12 flex flex-col justify-center items-center">
+                <div class="mb-8">
+                    <img class="object-center object-cover rounded-full h-36 w-36" src="" alt="photo">
+                </div>
+                <div class="text-center">
+                    <p class="text-xl text-white font-bold mb-2">D2</p>
+                    <p class="text-base text-gray-400 font-normal"></p>
+                </div>
+            </div>
+            <div class="w-full bg-gray-900 rounded-lg sahdow-lg p-12 flex flex-col justify-center items-center">
+                <div class="mb-8">
+                    <img class="object-center object-cover rounded-full h-36 w-36" src="" alt="photo">
+                </div>
+                <div class="text-center">
+                    <p class="text-xl text-white font-bold mb-2">D3</p>
+                    <p class="text-base text-gray-400 font-normal"></p>
+                </div>
+            </div>
+            <div class="w-full bg-gray-900 rounded-lg sahdow-lg p-12 flex flex-col justify-center items-center">
+                <div class="mb-8">
+                    <img class="object-center object-cover rounded-full h-36 w-36" src="" alt="photo">
+                </div>
+                <div class="text-center">
+                    <p class="text-xl text-white font-bold mb-2">D4</p>
+                    <p class="text-base text-gray-400 font-normal"></p>
+                </div>
+            </div>
+            <div class="w-full bg-gray-900 rounded-lg sahdow-lg p-12 flex flex-col justify-center items-center">
+                <div class="mb-8">
+                    <img class="object-center object-cover rounded-full h-36 w-36" src="" alt="photo">
+                </div>
+                <div class="text-center">
+                    <p class="text-xl text-white font-bold mb-2">D5</p>
+                    <p class="text-base text-gray-400 font-normal"></p>
+                </div>
+            </div>
+            <div class="w-full bg-gray-900 rounded-lg sahdow-lg p-12 flex flex-col justify-center items-center">
+                <div class="mb-8">
+                    <img class="object-center object-cover rounded-full h-36 w-36" src="" alt="photo">
+                </div>
+                <div class="text-center">
+                    <p class="text-xl text-white font-bold mb-2">D6</p>
+                    <p class="text-base text-gray-400 font-normal"></p>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+</template>
+
+<script>
+export default {}
+</script>
