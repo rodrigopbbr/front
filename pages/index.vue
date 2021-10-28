4a25036cd9028bf04b30a79d0bd2b8979c31e2b6
Rodrigo Pereira <rodrigopbbr@gmail.com>
2021-10-28 18:54:27 -0300

    Atualização do index e inserção de imagens.

--- pages/index.vue
+++ pages/index.vue
@@ -1,66 +1,67 @@
 <template>
- <div class="w-full bg-gray-800">
+ <div class="w-full bg-white">
     <section class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 py-12">
         <div class="text-center pb-12">
           
-            <h1 class="font-bold text-3xl md:text-4xl lg:text-5xl font-heading text-white">
-                Ambiente Teste            
+            <h1 class=" bg-white font-bold text-3xl md:text-4xl lg:text-5xl font-heading text-black shadow-2xl ">
+                Ambiente Teste           
             </h1>
         </div>
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            <div class="w-full bg-gray-900 rounded-lg sahdow-lg p-12 flex flex-col justify-center items-center">
+            <div class="w-full bg-white rounded-lg sahdow-lg p-12 flex flex-col justify-center items-center shadow-2xl">
                 <div class="mb-8">
-                    <img class="object-center object-cover rounded-full h-36 w-36" src="" alt="photo">
+                    <img class="object-center object-cover rounded-full h-36 w-36" src="~/assets/images/comprovante-residencia.png" alt="">
                     
                 </div>
                 <div class="text-center">
-                    <p class="text-xl text-white font-bold mb-2">D1</p>
-                    <p class="text-base text-gray-400 font-normal"></p>
+                    <p class="text-md text-black font-bold mb-2">COMPROVANTE DE REDIDÊNCIA</p> 
                 </div>
+                
+                
             </div>
-            <div class="w-full bg-gray-900 rounded-lg sahdow-lg p-12 flex flex-col justify-center items-center">
+            <div class="w-full bg-white rounded-lg sahdow-lg p-12 flex flex-col justify-center items-center shadow-2xl">
                 <div class="mb-8">
-                    <img class="object-center object-cover rounded-full h-36 w-36" src="" alt="photo">
+                    <img class="object-center object-cover rounded-full h-36 w-36" src="~/assets/images/rg.png" alt="">
                 </div>
                 <div class="text-center">
-                    <p class="text-xl text-white font-bold mb-2">D2</p>
-                    <p class="text-base text-gray-400 font-normal"></p>
+                    <p class="text-md text-black font-bold mb-2">REGISTRO DE IDENTIDADE</p>
+                    
                 </div>
             </div>
-            <div class="w-full bg-gray-900 rounded-lg sahdow-lg p-12 flex flex-col justify-center items-center">
+            <div class="w-full bg-white rounded-lg sahdow-lg p-12 flex flex-col justify-center items-center shadow-2xl">
                 <div class="mb-8">
-                    <img class="object-center object-cover rounded-full h-36 w-36" src="" alt="photo">
+                    <img class="object-center object-cover rounded-full h-36 w-36" src="" alt="">
                 </div>
                 <div class="text-center">
-                    <p class="text-xl text-white font-bold mb-2">D3</p>
-                    <p class="text-base text-gray-400 font-normal"></p>
+                    <p class="text-xl text-black font-bold mb-2">CNH</p>
+                    
                 </div>
             </div>
-            <div class="w-full bg-gray-900 rounded-lg sahdow-lg p-12 flex flex-col justify-center items-center">
+            <div class="w-full bg-white rounded-lg sahdow-lg p-12 flex flex-col justify-center items-center shadow-2xl">
                 <div class="mb-8">
-                    <img class="object-center object-cover rounded-full h-36 w-36" src="" alt="photo">
+                    <img class="object-center object-cover rounded-full h-36 w-36" src="" alt="">
                 </div>
                 <div class="text-center">
-                    <p class="text-xl text-white font-bold mb-2">D4</p>
-                    <p class="text-base text-gray-400 font-normal"></p>
+                    <p class="text-xl text-black font-bold mb-2">CUPOM FISCAL</p>
+                    
                 </div>
             </div>
-            <div class="w-full bg-gray-900 rounded-lg sahdow-lg p-12 flex flex-col justify-center items-center">
+            <div class="w-full bg-white rounded-lg sahdow-lg p-12 flex flex-col justify-center items-center shadow-2xl">
                 <div class="mb-8">
-                    <img class="object-center object-cover rounded-full h-36 w-36" src="" alt="photo">
+                    <img class="object-center object-cover rounded-full h-36 w-36" src="" alt="">
                 </div>
                 <div class="text-center">
-                    <p class="text-xl text-white font-bold mb-2">D5</p>
-                    <p class="text-base text-gray-400 font-normal"></p>
+                    <p class="text-xl text-black font-bold mb-2">NOTA FISCAL</p>
+                    
                 </div>
             </div>
-            <div class="w-full bg-gray-900 rounded-lg sahdow-lg p-12 flex flex-col justify-center items-center">
+            <div class="w-full bg-white rounded-lg sahdow-lg p-12 flex flex-col justify-center items-center shadow-2xl">
                 <div class="mb-8">
-                    <img class="object-center object-cover rounded-full h-36 w-36" src="" alt="photo">
+                    <img class="object-center object-cover rounded-full h-36 w-36" src="" alt="">
                 </div>
                 <div class="text-center">
-                    <p class="text-xl text-white font-bold mb-2">D6</p>
-                    <p class="text-base text-gray-400 font-normal"></p>
+                    <p class="text-xl text-black font-bold mb-2">NFE</p>
+                    
                 </div>
             </div>
         </div>
